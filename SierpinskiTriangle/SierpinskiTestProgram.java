a17bd1d61cf2f09383f4a25818992f2568806346
nivanov344 <72230086+nivanov344@users.noreply.github.com>
2021-02-25 23:15:22 +0200

    Create SierpinskiTestProgram.java

---
+++ SierpinskiTriangle/SierpinskiTestProgram.java
@@ -0,0 +1,14 @@
+package schl;
+
+import javax.swing.JFrame;
+
+public class SierpinskiTestProgram {
+	public static void main(String[] args) {
+		SierpinskiTriangleFrame frame = new SierpinskiTriangleFrame();
+		frame.setSize(400, 400);
+		frame.setTitle("TestFigurePanel");
+		frame.setLocationRelativeTo(null); // Center the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
