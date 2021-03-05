78bea65cce1f1a17ef54048aec50e577062e94ec
nivanov344 <72230086+nivanov344@users.noreply.github.com>
2021-03-05 08:00:28 +0200

    Create GenericStack.java

---
+++ GenericStack/GenericStack.java
@@ -0,0 +1,43 @@
+package schl;
+
+public class GenericStack<E> {
+	private E[] list = (E[]) new Object[3];
+	private int size = 0;
+	public GenericStack() {
+		
+	}
+	
+	public int getSize() {
+		return size;
+	}
+
+	public E peek() {
+		return list[size-1];
+	}
+
+	public void push(E o) {
+		if (size >= list.length) {
+			doubleList();
+		}
+		list[size++] = o;
+	}
+
+	public E pop() {
+		E o = list[--size];
+		return o;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	private void doubleList() {
+		E[] tempList = (E[]) new Object[list.length * 2];
+		System.arraycopy(list, 0, tempList, 0, list.length);
+		list = tempList;
+	}
+	@Override
+	public String toString() {
+		return "stack: " + list.toString();
+	}
+
+}
