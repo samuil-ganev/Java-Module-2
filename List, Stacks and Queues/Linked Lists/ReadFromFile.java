1665f7c9151f65c3ed421b763a7dbc6aabe5d7a7
nivanov344 <72230086+nivanov344@users.noreply.github.com>
2021-03-24 10:01:44 +0200

    Update ReadFromFile.java

--- List, Stacks and Queues/Linked Lists/ReadFromFile.java
+++ List, Stacks and Queues/Linked Lists/ReadFromFile.java
@@ -5,13 +5,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 
-public class ReadFromFile {
+public class zad2 {
 
 	public static void main(String[] args) throws IOException {
-		String fileName = "test.txt";
+		String fileName = "src/queues/test.txt";
 		String line;
 	    File file = new File(fileName);
 	    FileReader f = new FileReader(file);
@@ -20,7 +21,8 @@ public class ReadFromFile {
 	    LinkedList<String> ll = new LinkedList<String> ();
 	    
 	    while((line = b.readLine()) != null) {
-	    	ll.add(line);
+	    	String lineWords[] = line.split("[\\s.,]+");
+	    	ll.addAll(Arrays.asList(lineWords));
 	    }
 	    
 	    Collections.sort(ll);
@@ -28,3 +30,6 @@ public class ReadFromFile {
 	}
 
 }
+
+ 
+
